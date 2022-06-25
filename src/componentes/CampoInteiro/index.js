7d58b36338eb635352763a9b8f637fd3b8f09150
AshileySabah <ashileysabrina2002@gmail.com>
2022-06-25 19:35:01 -0300

    feat; estilos componente input

--- src/componentes/CampoInteiro/index.js
+++ src/componentes/CampoInteiro/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {TextInput} from 'react-native';
+import estilosPadrao from './estilos';
 
-export default function CampoInteiro({valor, acao}) {
+export default function CampoInteiro({valor, acao, estilos}) {
+  const atualiza = novoValor => {
+    const verificaInteiro = novoValor.match(/^[0-9]*$/);
+    if (!verificaInteiro) {
+      return;
+    }
+
+    const removeZeroEsquerda = novoValor.replace(/^(0)(.+)/, '$2');
+    acao(removeZeroEsquerda);
+  };
+
   const numeroEmTexto = String(valor);
 
   return (
     <TextInput
+      style={[estilosPadrao.campo, estilos]}
       keyboardType="number-pad"
       selectTextOnFocus
       value={numeroEmTexto}
-      onChangeText={novoValor => acao(novoValor)}
+      onChangeText={novoValor => atualiza(novoValor)}
     />
   );
 }
